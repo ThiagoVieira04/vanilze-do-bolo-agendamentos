4bc3b07f10b5d8ae5ca78d85f5ab50d8e70dcb7a
ThiagoVieira04 <tsmv05@hotmail.com>
2025-09-15 20:49:31 -0300

    update apk

---
+++ app/src/main/java/app/web/vanilze_do_bolo_agendamentos_5620e/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.web.vanilze_do_bolo_agendamentos_5620e.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
